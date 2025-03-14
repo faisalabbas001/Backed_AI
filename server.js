eb34c2742bc5f320f8a4bfa68b671346f23f6ee2
faisalabbas001 <faisalabbas7959@gmail.com>
2025-03-14 09:41:19 +0500

    update service.js file

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
-require('dotenv').config()
-const app = require('./src/app')
+require('dotenv').config();
+const app = require('./src/app');
 
-
-
-app.listen(3000, () => {
-    //console.log('Server is running on http://localhost:3000')
-})
+// ❌ REMOVE app.listen(3000) for Vercel
+module.exports = app;  // ✅ Correct way for Vercel
