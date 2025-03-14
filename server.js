c205ba745b30afe83c343b8771a44a808f5fa682
faisalabbas001 <faisalabbas7959@gmail.com>
2025-03-14 21:07:55 +0500

    update that the file is vercel.json file

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
-const app = require('./src/app')
+require('dotenv').config();
+const app = require('./src/app');
 
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
-app.listen(3000, () => {
-    // console.log('Server is running on http://localhost:3000')
-})
-
-module.exports=app;
+module.exports = app;
